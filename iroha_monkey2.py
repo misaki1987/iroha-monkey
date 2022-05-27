e43f260522d31fd4e4160afb51af3c1eb768f0ee
misaki1987 <misaki.asahilaw@gmail.com>
2022-05-27 09:55:42 +0900

    Create iroha_monkey2.py

---
+++ iroha_monkey2.py
@@ -0,0 +1,45 @@
+import random
+
+#文字リストの生成
+chara = ["い","ろ","は","に","ほ","へ","と","ち","り","ぬ","る","を",
+"わ","か","よ","た","れ","そ","つ","ね","な","ら","む",
+"う","ゐ","の","お","く","や","ま","け","ふ","こ","え","て",
+"あ","さ","き","ゆ","め","み","し","ゑ","ひ","も","せ","す"]
+
+#正しいいろは歌を作る
+iroha = "".join(chara)
+
+#カウンターに０をセット
+counter = 0
+
+#文章作成関数を定義
+def make_iroha() :
+    list = []
+    #文字リストを使い切るまでループ
+    while len(list) < len(chara):
+        x = random.choice(chara)
+        #重複した文字だった場合は追加しない
+        if not x in list:
+            list.append(x)
+    
+    sentence = "".join(list)
+    return sentence
+
+#正解の文字列ができるまで文章を生成し続ける
+while True:
+    result = make_iroha()
+    counter = counter + 1
+    #結果の出力
+    print("試行回数"+str(counter)+"回目")
+    print("今回の結果は、"+"「"+ result + "」")
+    print("monkeys keep trying...")
+
+    #正解が見つかったらループから抜ける
+    if result == iroha:
+        break
+
+#結果の出力
+print("!!!!!!!!!!!!!!!")
+print("monkeys found it!")
+
+
